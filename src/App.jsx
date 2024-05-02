55813d5d93d86cbcb44b64a9f3a49c0d6e3d5e6d
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-05-02 17:43:04 +0530

    nav structure done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div>
+    <div className="text-black">
       <Navbar />
     </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 function Navbar() {
-  return <div className="text-black"></div>;
+  return (
+    <nav className="bg-purple-400 flex items-center justify-between px-10 py-5">
+      <div className="text-center font-semibold text-xl ">PassOp</div>
+      <ul>
+        <li className="flex gap-8 ">
+          <a
+            href=""
+            className="capitalize hover:font-bold font-semibold italic text-emerald-800"
+          >
+            HOME
+          </a>
+          <a
+            href=""
+            className="uppercase hover:font-bold font-semibold italic text-emerald-800"
+          >
+            About
+          </a>
+          <a
+            href=""
+            className="uppercase hover:font-bold font-semibold italic text-emerald-800"
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
